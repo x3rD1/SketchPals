fec27f387159279a7fb50453de4915353b1dc71d
snowfxl <rexxalfaro+github@gmail.com>
2026-05-27 20:40:17 -0700

    feat(api): add GET and PATCH handlers for canvas

---
+++ frontend/src/api/canvas.ts
@@ -0,0 +1,41 @@
+import type { Stroke } from "../canvas/types";
+
+export const getCanvasById = async (id: string | undefined) => {
+  const res = await fetch(`http://localhost:3000/canvas/${id}`);
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(data.message);
+
+  return data;
+};
+
+export const createCanvas = async () => {
+  const res = await fetch("http://localhost:3000/canvas", { method: "POST" });
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(data.message);
+
+  return data;
+};
+
+export const saveCanvas = async (
+  id: string,
+  strokes: Stroke[],
+  version: number,
+) => {
+  const res = await fetch(`http://localhost:3000/canvas/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ strokes, version }),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(data.message);
+
+  return data;
+};
