c98895711d1e01de96dd984f45ed72ec3bbfaf89
snowfxl <rexxalfaro+github@gmail.com>
2026-06-13 18:32:18 -0700

    refactor: clean up Canvas component by removing unrelated logic

--- frontend/src/features/canvas/components/Canvas.tsx
+++ frontend/src/features/canvas/components/Canvas.tsx
@@ -1,66 +1,27 @@
-import SaveButton from "./SaveButton";
-import useCanvasEngine from "../hooks/useCanvasEngine";
-import useCanvasTools from "../hooks/useCanvasTools";
+import styles from "./Canvas.module.css";
+import type { CanvasEngine, ToolEngine } from "../types/types";
 
-function Canvas() {
-  const engine = useCanvasEngine();
+type CanvasProps = {
+  engine: CanvasEngine;
+  tool: ToolEngine;
+};
 
-  const tool = useCanvasTools(engine);
-
+function Canvas({ engine, tool }: CanvasProps) {
   return (
-    <>
-      {engine.id && (
-        <SaveButton
-          id={engine.id}
-          strokes={engine.history.strokes}
-          version={engine.data.version}
-          setVersion={engine.data.setVersion}
-          generateThumbnail={engine.canvas2D.generateThumbnail}
-        />
-      )}
-
+    <div className={styles.canvasWrapper}>
       <canvas
+        className={styles.canvas}
         onContextMenu={(e) => e.preventDefault()}
         style={{ border: "1px solid red", cursor: tool.cursorStyle }}
-        width={500}
-        height={500}
+        width={1280}
+        height={720}
         ref={engine.canvas2D.canvasRef}
         onMouseDown={tool.handleMouseDown}
         onMouseMove={tool.handleMouseMove}
         onMouseUp={tool.handleMouseUp}
         onMouseLeave={tool.handleMouseLeave}
       />
-      <button
-        onClick={engine.history.handleUndo}
-        disabled={engine.history.state.index === 0}
-      >
-        Undo
-      </button>
-      <button
-        onClick={engine.history.handleRedo}
-        disabled={
-          engine.history.state.history.length === engine.history.state.index + 1
-        }
-      >
-        Redo
-      </button>
-      <button onClick={() => tool.selectionTool("pen")}>Pencil</button>
-      <button onClick={() => tool.selectionTool("eraser")}>Eraser</button>
-      <input
-        type="color"
-        value={engine.stroke.color}
-        onChange={(e) => engine.stroke.setColor(e.target.value)}
-      />
-      <input
-        type="range"
-        value={engine.stroke.width}
-        min={1}
-        max={20}
-        onChange={(e) => engine.stroke.setWidth(Number(e.target.value))}
-      />
-      <button onClick={() => tool.selectionTool("pan")}>Pan</button>
-      <button onClick={() => tool.selectionTool("select")}>Select</button>
-    </>
+    </div>
   );
 }
 
