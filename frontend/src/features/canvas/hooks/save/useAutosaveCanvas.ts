332bcce71c771b54e644b2a59edcdac5516ea101
snowfxl <rexxalfaro+github@gmail.com>
2026-06-24 00:00:19 -0700

    feat: create custom hook for autosaving canvas

---
+++ frontend/src/features/canvas/hooks/save/useAutosaveCanvas.ts
@@ -0,0 +1,44 @@
+import { useCallback, useRef } from "react";
+import type { CanvasEngine, SaveCanvas } from "../../types/types";
+import compact from "../../utils/compact";
+
+type useAutosaveCanvasVars = {
+  save: SaveCanvas;
+  engine: CanvasEngine;
+};
+
+function useAutosaveCanvas({ save, engine }: useAutosaveCanvasVars) {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const commitSave = useCallback(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = null;
+    }
+
+    // Capture a snapshot of engine.canvasOpsQueueRef.current and clear it
+    const drainedOps = engine.drainOps();
+    if (drainedOps.length === 0) return;
+
+    const compactedOps = compact(drainedOps);
+
+    // Creates a thumbnail url on save
+    const thumbnail = engine.canvas2D.generateThumbnail();
+
+    save.mutate({ ops: compactedOps, thumbnail });
+  }, [save, engine]);
+
+  const scheduleAutosave = useCallback(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      commitSave();
+    }, 5000);
+  }, [commitSave]);
+
+  return { scheduleAutosave, commitSave };
+}
+
+export default useAutosaveCanvas;
