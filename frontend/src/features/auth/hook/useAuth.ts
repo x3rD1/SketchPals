30612ebd5cff9a43772090755c7437c18bc8a543
snowfxl <rexxalfaro+github@gmail.com>
2026-06-29 19:58:48 -0700

    refactor: set useQuery retry to false

--- frontend/src/features/auth/hook/useAuth.ts
+++ frontend/src/features/auth/hook/useAuth.ts
@@ -2,7 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchMe } from "../api/fetchMe";
 
 function useAuth() {
-  const { data, isPending } = useQuery({ queryKey: ["me"], queryFn: fetchMe });
+  const { data, isPending } = useQuery({
+    queryKey: ["me"],
+    queryFn: fetchMe,
+    retry: false,
+  });
 
   return { isPending, isAuthenticated: !!data?.user };
 }
